f244d775337d5d751068537680adfaf84e529ae0
Leona Juan <leonajuan@gmail.com>
2023-01-09 20:31:06 -0500

    some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 function Header() {
   return (
-    <h1>Leona Juan</h1>
+    <div>
+      <h1 className='header'>Leona Juan</h1>
+      <h4 className='subheader'>Full Stack Software Engineer ♡ JavaScript ♡ React ♡ Ruby on Rails</h4>
+    </div>
   )
 }
 
