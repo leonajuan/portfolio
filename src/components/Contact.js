9c47bc36150ca54d97e28b2755a830fac325fc84
Leona Juan <leonajuan@gmail.com>
2023-01-10 20:12:32 -0500

    configured emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,37 @@
-function Contact() {
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+// function Contact() {
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('contact_service', 'contact_form', form.current, 'a8K2eQ-yqxPWaBEvK')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div>
-      <form className='contact-form'>
+      <form className='contact-form' ref={form} onSubmit={sendEmail}>
         <h4>Get In Touch With Me!</h4>
-        <label htmlFor='firstName'>First Name</label>
-        <input className='form-input' type='text' name='firstName' />
-        <label htmlFor='lastName'>Last Name</label>
-        <input className='form-input' type='text' name='lastName' />
-        <label htmlFor='email'>Email</label>
-        <input className='form-input' type='text' name='email' />
-        <label htmlFor='message'>Message</label>
-        <input className='form-input' type='text' name='message' />
+        <label>Name</label>
+        <input className='form-input' type='text' name='user_name' />
+        <label>Email</label>
+        <input className='form-input' type='text' name='user_email' />
+        <label>Message</label>
+        <textarea className='form-input' name='message' />
         <button className='submit-button'>Submit</button>
       </form>
     </div>
   )
 }
+// }
 
-export default Contact;
+export default ContactUs;
